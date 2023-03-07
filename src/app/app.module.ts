b8a38f2030125802e60d528e78c402d60edb2c55
Sebafleaca <sebastian.fleaca@hotmail.com>
2023-03-07 17:29:13 +0100

    create a first Angular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScientistsComponent } from './scientists/scientists.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScientistsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/scientists/scientists.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scientists',
+  templateUrl: './scientists.component.html',
+  styleUrls: ['./scientists.component.css']
+})
+export class ScientistsComponent {
+
+}
