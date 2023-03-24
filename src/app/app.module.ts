9448d5664de9c95adcf32415611393641f60a128
Sebafleaca <sebastian.fleaca@hotmail.com>
2023-03-24 09:48:32 +0100

    implement message service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScientistsComponent } from './scientists/scientists.component';
 import { ScientistDetailComponent } from './scientist-detail/scientist-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ScientistsComponent,
-    ScientistDetailComponent
+    ScientistDetailComponent,
+    MessagesComponent
   ],
   imports: [
     FormsModule,
