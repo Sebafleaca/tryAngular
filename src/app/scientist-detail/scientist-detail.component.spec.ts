6cf34e19095cfab5aca91ec852f68ea190db7987
Sebafleaca <sebastian.fleaca@hotmail.com>
2023-05-11 10:37:30 +0200

    implement some tests for ScientistDetailComponent

--- src/app/scientist-detail/scientist-detail.component.spec.ts
+++ src/app/scientist-detail/scientist-detail.component.spec.ts
@@ -1,15 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { ScientistDetailComponent } from './scientist-detail.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('ScientistDetailComponent', () => {
   let component: ScientistDetailComponent;
   let fixture: ComponentFixture<ScientistDetailComponent>;
+  let h3: HTMLElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       imports: [
         HttpClientModule, 
         RouterModule.forRoot([])
@@ -17,13 +20,27 @@ describe('ScientistDetailComponent', () => {
       declarations: [ ScientistDetailComponent ]
     })
     .compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ScientistDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    fixture.detectChanges();
+    expect(component).toBeDefined();
   });
+
+  it('expect right title, with querySelector()', () => {
+    h3 = fixture.nativeElement.querySelector('#title');
+    expect(h3.textContent).toContain('Details');
+  })
+
+  it('expect title to be blue, with By.css()', () => {
+    let css: DebugElement = fixture.debugElement.query(By.css('#title'));
+    let p: HTMLElement = css.nativeElement;
+    expect(p.getAttribute('style')).toContain('blue');
+  })
 });
